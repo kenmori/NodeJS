3aa503a5067f306966e96f2868ea53e4d432132f
kenji <kennjimoritata@yahoo.co.jp>
2016-07-02 19:14:00 +0900

    exports require

---
+++ src/my_module/test-currency.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Created by No51 on 2016/07/02.
+ */
+
+var currency = require('./currency');
+
+console.log('50 Canadian dollars equals this amount of US dollars');
+
+console.log(currency.canadianToUS(50));
+console.log('30 US dollars equals this amount of Canadian dollars');
+
+console.log(currency.USToCanadian(30));
+
+
