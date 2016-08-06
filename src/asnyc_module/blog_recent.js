506adfcfc91337d852c9f92e2560b1765bba6c7e
kenji <kennjimoritata@yahoo.co.jp>
2016-08-06 15:11:36 +0900

    fix nest callback

--- src/asnyc_module/blog_recent.js
+++ src/asnyc_module/blog_recent.js
@@ -1,26 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
-	if(req.url  == '/'){
-		fs.readFile('./titles.json', function(err, data){
-			if (err) {
-				console.log(err);
-				res.end('Server Error');
-			} else {
-				var titles = JSON.parse(data.toString());
-				fs.readFile('./template.html', function(err, data){ 
-					if(err {
-						console.log(err);
-						res.end('Server Error');
-					} else {
-						var tmpl = data.toStoring();
-						var html = tmpl.replace('%', titles.json('</li><li>'));
-						res.writeHead(200, {'Content-Type' : 'text/html'});
-						res.end(html);//htmlをユーザーに送る
-					}
-				});
-			}
-		});
-	}	
+http.createServer(
+	function(req, res){
+	getTitle(res);
 }).listen(8000, "127.0.0.1");
+
+function getTitle (res) {
+fs.readFile('./title.json', function(err, data){
+	if(err) {
+		hadError(err, res);
+	} else {
+		var titles = JSON.parse(data.toStoring());
+		getTemplate(titles,res);
+	}
+	});
+};
+function getTemplate(titles, res){
+	fs.readFile('./template.html', function(err, data){
+		if(err) {
+			hadError(err, res);
+		} else {
+			formatHtml(titles, data.toStoring(), res);
+		}
+	});
+};
+
+function formatHtml(titles, tmpl, res){
+	var html = tmpl.replace('%', titles.json('</li><li>'));
+	res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.end(html);//htmlをユーザーに送る
+};
+function hadError(err, res) {
+	console.log(err);
+	res.end('server Error');
+};
