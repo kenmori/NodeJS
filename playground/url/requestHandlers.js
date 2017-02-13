adc976d3dc3854efd9d95747142f0c9a6e301ef7
森田賢二 <kenjimoritata@gmail.com>
2017-02-13 20:40:48 +0900

    Fix bug

--- playground/url/requestHandlers.js
+++ playground/url/requestHandlers.js
@@ -1,13 +1,21 @@
+var exec = require("child_process").exec;
 
 
-
-function start(){
+function start(response){
     console.log("Request handler 'start' was called");
+    exec("ls -lah", function(error, stdout, stderr){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    })
 }
 
 
-function upload(){
+function upload(response){
     console.log("Request handler 'upload' was called");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Update");
+    response.end();
 }
 
 exports.start = start;
