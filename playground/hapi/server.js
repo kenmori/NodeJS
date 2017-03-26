2b86c8ea5aa7f9696631edb15fff0e2cde5a94df
森田賢二 <kenjimoritata@gmail.com>
2017-03-26 12:47:25 +0900

    create route

--- playground/hapi/server.js
+++ playground/hapi/server.js
@@ -1,3 +1,35 @@
-/**
- * Created by moritakenji on 2017/03/26.
- */
+'use strict';
+
+const Hapi = require('hapi');
+
+
+const server = new Hapi.Server();
+
+server.connection({port: 3000, host: 'localhost'});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply){
+        reply('Hell, world');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function(request, reply){
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+})
+
+server.start(function(err){
+    if(err){
+        throw err;
+    }
+    console.log('Server running');
+});
+
+
+
+
