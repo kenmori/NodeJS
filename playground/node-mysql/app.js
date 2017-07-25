2f2ca13fe7588d57509f50de37a9ec48298e8209
森田賢二 <kenjimoritata@gmail.com>
2017-07-26 08:03:29 +0900

    Node-mysql

---
+++ playground/node-mysql/app.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'locaLhosT',
+    user: 'user',
+    password: 'password',
+    database: 'database name'
+});
+connection.connect((err) => {
+    if (err) throw err;
+console.log('Connected!');
+});
+
+
+
+
+
+
+
