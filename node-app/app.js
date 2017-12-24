3ef20191b5225b9f66fb5b5e54d348b201a7d00e
森田賢二 <kenjimoritata@gmail.com>
2017-12-25 07:47:25 +0900

    add ejs

--- node-app/app.js
+++ node-app/app.js
@@ -1,12 +1,33 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+const url = require('url');
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_page = fs.readFileSync('./style.css', 'utf8');
 
 function getFromClient(request, response){
-        fs.readFile('index.html', 'UTF-8', (error, data)=>{
+            var urlparse = url.parse(request.url);
+    switch (urlparse.pathname) {
+        case '/':
+
+            var content = ejs.render(index_page, {
+                title: "Indexページ",
+                content: "これはテンプレートを使ったサンプルページです"
+            });
             response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end();
-    });
+            response.write(content);
+            response.end();
+            break;
+        case '/style.css':
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(style_page);
+            response.end();
+            break;
+        default:
+            response.writeHead(200, { 'Content-Type' : 'text/plain'});
+            response.end('no page');
+            break;
+    }
 }
 
 var server = http.createServer(getFromClient);
