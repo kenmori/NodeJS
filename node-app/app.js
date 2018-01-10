2f391e741ac3e0b4cbe6bacd9bae3ecc361c1514
森田賢二 <kenjimoritata@gmail.com>
2018-01-10 13:51:03 +0900

    Add file

--- node-app/app.js
+++ node-app/app.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer((request, response) => {
-        response.end('Hello Node.js!');
+function getFromClient(request, response){
+
+    fs.readFile('./index.html', 'UTF-8', (error, data) => {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
     });
+}
+var server = http.createServer(getFromClient);
 
 server.listen(3000);
