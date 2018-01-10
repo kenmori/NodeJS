4bf5c181c08a01fdb325fc2f2120c81642a3395d
森田賢二 <kenjimoritata@gmail.com>
2018-01-10 14:11:14 +0900

    replace

--- node-app/app.js
+++ node-app/app.js
@@ -1,14 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
-function getFromClient(request, response){
-        fs.readFile('index.html', 'UTF-8', (error, data)=>{
-            response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end();
-    });
-}
 
 var server = http.createServer(getFromClient);
 
 server.listen(3000);
+
+
+//ここまでメインプログラム
+function getFromClient(request, response){
+    fs.readFile('index.html', 'UTF-8', (error,data) => {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data.replace(/dummy_title/g, 'タイトル').replace(/dummy_content/g, 'これがコンテンツです'));
+        response.end();
+    });
+}
+
