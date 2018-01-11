e3d28d5e8766cce3ae694363c4e845b11d44d28f
森田賢二 <kenjimoritata@gmail.com>
2018-01-11 17:55:45 +0900

    fix

--- express-app/index.js
+++ express-app/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
+app.engine('ejs', ejs.renderFile);
 app.get('/', (req, res) => {
     res.render('index.ejs', {title: 'Index', content:'This is Express-app Top page!'});
 });
 
 app.listen(3000, ()=> {
     console.log('Start server port:3000');
-})
+});
+
+
 
 
