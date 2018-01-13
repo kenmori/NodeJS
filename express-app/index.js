86c5d3ab5891233d411cd49112e391a99023a29d
森田賢二 <kenjimoritata@gmail.com>
2018-01-13 20:05:55 +0900

    Create other

--- express-app/index.js
+++ express-app/index.js
@@ -3,11 +3,17 @@ var ejs = require('ejs');
 var app = express();
 
 app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.render('index.ejs', {title: 'Index', content:'This is Express-app Top page!'});
+	var msg = 'This is Express Page!<br>' + 'これはスタイルシートを利用した例です';
+    res.render('index.ejs', {title: 'Index', content: msg, link: {text: '別ページに移動', href:'/other' } });
 });
 
-app.listen(3000, ()=> {
+app.get('/other', (req, res) => {
+	var msg = 'This is Express Page!<br>' + 'これはotherページです';
+    res.render('index.ejs', {title: 'other', content: msg, link: {text: '別ページに移動', href:'/' } });
+});
+var server = app.listen(3000, ()=> {
     console.log('Start server port:3000');
 });
 
