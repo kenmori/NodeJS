6f82085bc93358d42b0d4cfbbbcf6f2d26a640f6
森田賢二 <kenjimoritata@gmail.com>
2018-01-16 11:52:35 +0900

    "add"

--- express-app/index.js
+++ express-app/index.js
@@ -4,12 +4,20 @@ var app = express();
 
 app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
+
+
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: false}));
 app.get('/', (req, res) => {
 	var url = '/other?name=fafa&pass=4890';
-	var msg = 'This is Express Page!<br>' + 'これはスタイルシートを利用した例です';
-    res.render('index.ejs', {title: 'Index', content: msg, link: {text: '別ページに移動', href:url} });
+	var msg = 'this is express page!<br>' + 'これはスタイルシートを利用した例です';
+    res.render('index.ejs', {title: 'index', content: msg, link: {text: '別ページに移動', href:url} });
 });
 
+app.post('/', (req, res) => {
+	var msg = 'this is posted page' + req.body.message + 'と送信しました';
+	res.render('index.ejs', {title: 'posted', content: msg});
+})
 app.get('/other', (req, res) => {
 	var name = req.query.name;
 	var pass = req.query.pass;
@@ -17,9 +25,5 @@ app.get('/other', (req, res) => {
     res.render('index.ejs', {title: 'other', content: msg, link: {text: '別ページに移動', href:'/' } });
 });
 var server = app.listen(3000, ()=> {
-    console.log('Start server port:3000');
+    console.log('start server port:3000');
 });
-
-
-
-
